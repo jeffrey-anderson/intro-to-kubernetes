96bc3fcd55f3571024c744e1cf29662d3ebe12f1
Jeff Anderson <jeff@jeff-anderson.com>
2022-03-02 20:46:44 -0500

    init db content

--- swe/init-swe-db.sh
+++ swe/init-swe-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres <<-EOSQL
 DROP TABLE if exists public.blog_post;
 DROP TABLE if exists public.author;
 
